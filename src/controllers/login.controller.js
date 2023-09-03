0c2b86d4540f0425e92976bc68c4faf06864942a
Luiz Silva <luiz.silva@grupozelo.com>
2023-09-03 00:38:51 -0300

    Implementando JWT E BCRYPT

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,5 +1,7 @@
-/* eslint-disable no-undef */
+const jwt = require('jsonwebtoken')
 const loginRepository = require('../repositories/login.repository')
+
+const secretKey = process.env.SECRET_KEY
 
 class LoginController {
     async buscaTodosUsuarios(_req, res) {
@@ -7,16 +9,19 @@ class LoginController {
         return res.send(login)
     }
 
-    async buscaUsuarioLoginSenha(req, res){
-          const { usuario, senha } = req.body
+    async buscaUsuarioLoginSenha(req, res) {
+        const { usuario, senha } = req.body
         const dadosWhere = {
-            usuario: usuario, 
+            usuario: usuario,
             senha: senha,
-            status: 1
+            status: 1,
         }
         const login = await loginRepository.buscaUsuarioLoginSenha(dadosWhere)
-      
-        return res.send(login)
+        if (!login) {
+            return res.status(401).json({ message: 'Senha incorreta' })
+        }
+        const token = jwt.sign(login, secretKey, { expiresIn: '1h' })
+        return res.send({ token })
     }
 }
 
--- src/repositories/login.repository.js
+++ src/repositories/login.repository.js
@@ -1,8 +1,7 @@
 const login = require('../models/login.model')
-const { Op } = require('sequelize')
 
 class LoginRepository {
-    async buscaTodosUsuarios(title) {
+    async buscaTodosUsuarios() {
         try {
             return await login.findAll()
         } catch (error) {
@@ -12,7 +11,10 @@ class LoginRepository {
 
     async buscaUsuarioLoginSenha(dadosWhere) {
         try {
-            return await login.findAll({ where: dadosWhere })
+            return await login.findOne({
+                raw: true,
+                where: dadosWhere,
+            })
         } catch (error) {
             throw new Error(error)
         }
