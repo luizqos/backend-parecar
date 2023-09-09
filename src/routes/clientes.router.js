32672e11809ffd11a5b12f1f6841c9a5bd057a2d
Luiz Silva <luiz.silva@grupozelo.com>
2023-09-09 15:40:30 -0300

    Renomeando rotas

--- src/routes/clientes.router.js
+++ src/routes/clientes.router.js
@@ -4,7 +4,7 @@ const router = require('express').Router()
 module.exports = (app) => {
     router.get('/', clientes.buscaClientes)
     router.post('/', clientes.insereCliente)
-    router.put('/id', clientes.atualizaCliente)
-    router.delete('/id', clientes.deletaCliente)
+    router.put('/', clientes.atualizaCliente)
+    router.delete('/', clientes.deletaCliente)
     app.use('/api/clientes', router)
 }
--- src/routes/estacionamentos.router.js
+++ src/routes/estacionamentos.router.js
@@ -3,9 +3,9 @@ const router = require('express').Router()
 
 module.exports = (app) => {
     router.get('/', estacionamentos.buscaTodosEstacionamentos)
-    router.get('/id', estacionamentos.buscaEstacionamentoPorId)
+    router.get('/', estacionamentos.buscaEstacionamentoPorId)
     router.post('/', estacionamentos.insereEstacionamento)
-    router.put('/id', estacionamentos.atualizaEstacionamento)
-    router.delete('/id', estacionamentos.deletaEstacionamento)
+    router.put('/', estacionamentos.atualizaEstacionamento)
+    router.delete('/', estacionamentos.deletaEstacionamento)
     app.use('/api/estacionamentos', router)
 }
