c1822a0261502e02163869faea6c8666199d2200
Luiz Silva <luiz.silva@grupozelo.com>
2023-09-13 07:29:03 -0300

    Ajustes em Login

--- src/repositories/login.repository.js
+++ src/repositories/login.repository.js
@@ -9,11 +9,22 @@ class LoginRepository {
         }
     }
 
-    async buscaUsuario(dadosWhere) {
+    async buscaLogin(filtros) {
+        try {
+            return await login.findAll({
+                raw: true,
+                where: filtros,
+            })
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async buscaUsuario(filtrosBuscaLogin) {
         try {
             return await login.findOne({
                 raw: true,
-                where: dadosWhere,
+                where: filtrosBuscaLogin,
             })
         } catch (error) {
             throw new Error(error)
