61b57031cb103306399e86cd4aaa5774b4249d00
Luiz Silva <luiz.silva@grupozelo.com>
2023-09-30 22:39:41 -0300

    Criando texto template

--- src/templates/confirmacaoSenha.js
+++ src/templates/confirmacaoSenha.js
@@ -3,10 +3,30 @@ const assunto = 'Confirmação de alteração de senha Parecar'
 const templateAlteraSenha = (hash) => {
     const urlEnvio = `${process.env.URL_FORGOT}${hash}`
     const corpoEmail = `<html>
+
     <head>
         <meta charset="UTF-8">
         <title>Confirmação de alteração de senha - Parecar</title>
+    
         <style>
+            .header {
+                padding: 10px;
+                text-align: left;
+            }
+    
+            .greeting {
+                line-height: 22.4px;
+                color: #212945;
+                font-size: 16px;
+                font-weight: 600;
+            }
+    
+            .message {
+                font-size: 16px;
+                line-height: 27.2px;
+                color: #212945;
+            }
+    
             .botao-confirmar {
                 background-color: #FFBE00;
                 border: none;
@@ -20,6 +40,27 @@ const templateAlteraSenha = (hash) => {
                 cursor: pointer;
                 border-radius: 10px;
             }
+    
+            .footer {
+                padding: 10px;
+                text-align: center;
+            }
+    
+            .footer-background {
+                background-color: #FFBE00;
+            }
+    
+            .footer p {
+                font-size: 12px;
+                line-height: 16.8px;
+                color: #000000;
+            }
+    
+            .auto-message {
+                font-size: 11px;
+                line-height: 16.8px;
+                color: #5c5959;
+            }
         </style>
     </head>
     
@@ -27,17 +68,15 @@ const templateAlteraSenha = (hash) => {
         <table style="width: 100%;">
             <tbody>
                 <tr>
-                    <td style="padding: 10px;" align="left">
-                        <br />
+                    <td class="header">
                         <div>
                             <br />
                             <p>
-                                <span
-                                    style="line-height: 22.4px; color: #212945; font-size: 16px; font-weight: 600;">Prezado(a),</span>
+                                <span class="greeting">Prezado(a).</span>
                             </p>
                             <p>
-                                <span style="font-size: 16px; line-height: 27.2px; color: #212945;">Clique no botão abaixo
-                                    para confirmar a alteração de senha do seu acesso ao Parecar</span>
+                                <span class="message">Clique no botão abaixo para confirmar a alteração de senha do seu
+                                    acesso ao Parecar</span>
                             </p>
                             <br />
                             <a href="${urlEnvio}" class="botao-confirmar">Confirmar</a>
@@ -53,29 +92,22 @@ const templateAlteraSenha = (hash) => {
                 <table style="width: 100%;">
                     <tbody>
                         <tr>
-                            <td style="padding: 10px;" align="left">
-                                <div style="background-color: #FFBE00;">
+                            <td class="footer">
+                                <div class="footer-background">
                                     <p style="text-align: center;">
-                                        <span style="font-size: 12px; line-height: 16.8px; color: #000000;">Parecar - Todos
-                                            os
-                                            direitos reservados</span>
+                                        <span class="copyright-text">Parecar - Todos os direitos reservados</span>
                                     </p>
                                 </div>
-                                    <p style="text-align: center;">
-                                        <span style="font-size: 11px; line-height: 16.8px; color: #5c5959;">Esta é uma
-                                            mensagem
-                                            automática, não é necessário respondê-la.</span>
-                                    </p>
+                                <p style="text-align: center;">
+                                    <span class="auto-message">Esta é uma mensagem automática, não é necessário
+                                        respondê-la.</span>
+                                </p>
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
-        </td>
-        </tr>
-        </tbody>
-        </table>
     </body>
     
     </html>`
